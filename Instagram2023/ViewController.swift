97524e1574a2dc23f81ab36afb3c99b73bc4f9ee
Berat <inceberat626@gmail.com>
2023-09-06 17:09:38 +0300

    Initial Commit

---
+++ Instagram2023/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram2023
+//
+//  Created by Berat Tugay İnce on 6.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
